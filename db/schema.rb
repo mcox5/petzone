aa93881b5dd06e9ca476ec810d8425c56fca5298
Tomas Casey <tomasmcasey@gmail.com>
2022-12-13 15:19:24 -0300

    Agregue arreglos en index y home

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_024552) do
     t.date "date"
     t.bigint "pet_id", null: false
     t.index ["pet_id"], name: "index_exams_on_pet_id"
-
   end
 
   create_table "meetings", force: :cascade do |t|
